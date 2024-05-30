cd68182fc2e3bb91c68ce499f347bd1152793a7c
Combro-Kim <rlawog@nate.com>
2024-05-30 15:57:39 +0900

    FireStore Module

--- app/src/main/java/kr/nbc/momo/data/di/FireBaseModule.kt
+++ app/src/main/java/kr/nbc/momo/data/di/FireBaseModule.kt
@@ -1,6 +1,8 @@
 package kr.nbc.momo.data.di
 
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,5 +17,15 @@ object FireBaseModule {
         firebaseDatabase.setPersistenceEnabled(true)
         firebaseDatabase.setPersistenceCacheSizeBytes(5000000)
         return firebaseDatabase
+    }
+
+
+    @Provides
+    fun provideFireBaseFireStore(): FirebaseFirestore {
+        val settings = FirebaseFirestoreSettings.Builder() //todo 캐시 설정
+            .build()
+        val firebaseFireStore = FirebaseFirestore.getInstance()
+        firebaseFireStore.firestoreSettings = settings
+        return firebaseFireStore
     }
 }
