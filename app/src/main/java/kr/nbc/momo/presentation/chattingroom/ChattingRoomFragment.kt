bbfea7ecb6c76e0fb130dca3dcf2c1396dc3ebe8
WinterWood0118 <dltjdals125@gmail.com>
2024-05-31 19:26:20 +0900

    feat: 튜터님 피드백 적용중

--- app/src/main/java/kr/nbc/momo/presentation/chattingroom/ChattingRoomFragment.kt
+++ app/src/main/java/kr/nbc/momo/presentation/chattingroom/ChattingRoomFragment.kt
@@ -24,8 +24,8 @@ class ChattingRoomFragment : Fragment() {
     private val groupId = "group_id"
 
     //공유 뷰모델에서 로그인 정보 받아오기
-    private val userId = "Winterwood"
-    private val userName = "김개똥"
+    private val userId = "Winterwood12"
+    private val userName = "이성민"
     private val rvAdapter = RVAdapter(userId)
 
     override fun onCreate(savedInstanceState: Bundle?) {
