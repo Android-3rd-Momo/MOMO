d8b23ab261fc40576e213fc31de7a118647b81c5
Combro-Kim <rlawog@nate.com>
2024-06-07 22:26:49 +0900

    refactor: UserRepository 코드 정리

--- app/src/main/java/kr/nbc/momo/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/kr/nbc/momo/data/repository/UserRepositoryImpl.kt
@@ -23,14 +23,27 @@ class UserRepositoryImpl @Inject constructor(
     private val storage: FirebaseStorage,
     private val userPreferences: UserPreferences //todo 보류
 ) : UserRepository {
+
+    //todo 중복으로 로그인 된 유저를 호출하는 부분 메서드 생성
+    //auth.currentUser.uid
+
+    //todo signUp, save 부분 줄이기 -> 메서드 생성
+    private suspend fun getCurrentUserUid(): String {
+        return auth.currentUser?.uid ?: throw Exception("User not login")
+    }
+
+    private suspend fun saveUserInfo(user: UserEntity) { //signUp, saveUserProfile
+        val currentUserUid = getCurrentUserUid()
+        val userResponse = user.toUserResponse()
+        fireStore.collection("userInfo").document(currentUserUid).set(userResponse).await()
+        userPreferences.saveUserInfo(user)
+    }
+
     override suspend fun signUpUser(email: String, password: String, user: UserEntity): UserEntity {
         return try {
             auth.createUserWithEmailAndPassword(email, password).await()
-            val currentUser = auth.currentUser ?: throw Exception("SignUp Failed")
-            val userResponse = user.toUserResponse()
-            fireStore.collection("userInfo").document(currentUser.uid).set(userResponse).await()
-            userPreferences.saveUserInfo(user) //dataStore
-            userResponse.toEntity()
+            saveUserInfo(user)
+            user
         } catch (e: Exception) {
             throw e
         }
@@ -39,8 +52,8 @@ class UserRepositoryImpl @Inject constructor(
     override suspend fun signInUser(email: String, password: String): UserEntity {
         return try {
             auth.signInWithEmailAndPassword(email, password).await()
-            val currentUser = auth.currentUser ?: throw Exception("SignIn Failed")
-            val snapshot = fireStore.collection("userInfo").document(currentUser.uid).get().await()
+            val currentUserUid = getCurrentUserUid()
+            val snapshot = fireStore.collection("userInfo").document(currentUserUid).get().await()
             val userResponse = snapshot.toObject(UserResponse::class.java) ?: throw Exception("Do not log in")
             userPreferences.saveUserInfo(userResponse.toEntity()) //dataStore
             userResponse.toEntity()
@@ -51,88 +64,60 @@ class UserRepositoryImpl @Inject constructor(
 
     override suspend fun saveUserProfile(user: UserEntity) { //저장
         try {
-            val currentUser = auth.currentUser ?: throw Exception("saveProfile Failed")
-
-            val profileImageUrl = if (user.userProfileThumbnailUrl.startsWith("content://")) {
-                val uri = Uri.parse(user.userProfileThumbnailUrl)
-                uploadStorage(storage.reference.child("userProfile/profile/${user.userId}.jpeg"), uri)
-            } else {
-                user.userProfileThumbnailUrl
-            }
-
-            val backgroundImageUrl = if (user.userBackgroundThumbnailUrl.startsWith("content://")) {
-                val uri = Uri.parse(user.userBackgroundThumbnailUrl)
-                uploadStorage(storage.reference.child("userProfile/background/${user.userId}.jpeg"), uri)
-            } else {
-                user.userBackgroundThumbnailUrl
-            }
-
-            val portfolioImageUrl = if (user.userPortfolioImageUrl.startsWith("content://")) {
-                val uri = Uri.parse(user.userPortfolioImageUrl)
-                uploadStorage(storage.reference.child("userProfile/portfolio/${user.userId}.jpeg"), uri)
-            } else {
-                user.userPortfolioImageUrl
-            }
-
             val updateUser = user.copy(
-                userProfileThumbnailUrl = profileImageUrl,
-                userBackgroundThumbnailUrl = backgroundImageUrl,
-                userPortfolioImageUrl = portfolioImageUrl
+                userProfileThumbnailUrl = uploadImageToStorage(user.userProfileThumbnailUrl,"profile"),
+                userBackgroundThumbnailUrl = uploadImageToStorage(user.userBackgroundThumbnailUrl,"background"),
+                userPortfolioImageUrl = uploadImageToStorage(user.userPortfolioImageUrl, "portfolio")
             )
-
-            val userResponse = updateUser.toUserResponse()
-            fireStore.collection("userInfo").document(currentUser.uid).set(userResponse).await()
-            userPreferences.saveUserInfo(user) //dataStore
+            saveUserInfo(updateUser)
         } catch (e: Exception) {
             throw e
         }
     }
 
-    private suspend fun uploadStorage(ref: StorageReference, uri: Uri): String {
-        return try {
-            val uploadTask = ref.putFile(uri)
-            val urlTask = uploadTask.continueWithTask { task ->
-                if (!task.isSuccessful) {
-                    task.exception?.let {
-                        throw it
+    private suspend fun uploadImageToStorage(imageUrl: String, path: String): String {
+        return if (imageUrl.startsWith("content://")) {
+            val uri = Uri.parse(imageUrl)
+            try {
+                val ref = storage.reference.child("userProfile/$path/${getCurrentUserUid()}.jpeg")
+                ref.putFile(uri).await()
+                ref.downloadUrl.await().toString()
+            } catch (e: Exception) {
+                throw e
+            }
+        } else{
+            imageUrl
+        }
+    }
+
+        override suspend fun isUserIdDuplicate(userId: String): Boolean {
+            return try {
+                val querySnapshot = fireStore.collection("userInfo")
+                    .whereEqualTo("userId", userId)
+                    .get()
+                    .await()
+                !querySnapshot.isEmpty
+            } catch (e: Exception) {
+                throw e
+            }
+        }
+
+        override fun getCurrentUser(): Flow<UserEntity?> = callbackFlow {
+            val currentUser = auth.currentUser
+            if (currentUser != null) {
+                val listenerRegistration = fireStore.collection("userInfo").document(currentUser.uid)
+                    .addSnapshotListener { snapshot, e -> //실시간 업데이트
+                        if (e != null) {
+                            close(e)
+                            return@addSnapshotListener
+                        }
+                        val userEntity = snapshot?.toObject(UserResponse::class.java)?.toEntity()
+                        trySend(userEntity).isSuccess
                     }
-                }
-                ref.downloadUrl
-            }.await()
-            urlTask.toString()
-        } catch (e: Exception) {
-            throw e
+                awaitClose { listenerRegistration.remove() }
+            } else {
+                trySend(null).isSuccess
+                close()
+            }
         }
     }
-
-    override suspend fun isUserIdDuplicate(userId: String): Boolean {
-        return try {
-            val querySnapshot = fireStore.collection("userInfo")
-                .whereEqualTo("userId", userId)
-                .get()
-                .await()
-            !querySnapshot.isEmpty
-        } catch (e: Exception) {
-            throw e
-        }
-    }
-
-    override fun getCurrentUser(): Flow<UserEntity?> = callbackFlow {
-        val currentUser = auth.currentUser
-        if (currentUser != null) {
-            val listenerRegistration = fireStore.collection("userInfo").document(currentUser.uid)
-                .addSnapshotListener { snapshot, e -> //실시간 업데이트
-                    if (e != null) {
-                        close(e)
-                        return@addSnapshotListener
-                    }
-                    val userEntity = snapshot?.toObject(UserResponse::class.java)?.toEntity()
-                    trySend(userEntity).isSuccess
-                }
-            awaitClose { listenerRegistration.remove() }
-        } else {
-            trySend(null).isSuccess
-            close()
-        }
-    }
-}
