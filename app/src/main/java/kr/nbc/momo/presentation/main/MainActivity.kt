0fab5485a2c1903ec573af94ac97005e41e65a9d
bonggyulim <qhdrbdhkdwks@naver.com>
2024-06-10 20:18:39 +0900

    fix : 단방향 해시함수를 양방향으로 바꿈

--- app/src/main/java/kr/nbc/momo/presentation/main/MainActivity.kt
+++ app/src/main/java/kr/nbc/momo/presentation/main/MainActivity.kt
@@ -56,7 +56,7 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 R.id.chattingListFragment -> {
-                    val chattingListFragment = SignUpFragment()
+                    val chattingListFragment = ChattingListFragment()
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.fragment_container, chattingListFragment)
                         .commit()
