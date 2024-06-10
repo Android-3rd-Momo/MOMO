3043cd1292e414cade1ed3af86ec4d38bb7e6e40
WinterWood0118 <dltjdals125@gmail.com>
2024-06-10 17:47:13 +0900

    feat: 검색 로직 변경

--- app/src/main/java/kr/nbc/momo/data/repository/SearchRepositoryImpl.kt
+++ app/src/main/java/kr/nbc/momo/data/repository/SearchRepositoryImpl.kt
@@ -11,16 +11,25 @@ import javax.inject.Inject
 class SearchRepositoryImpl @Inject constructor(
     private val fireStore: FirebaseFirestore
 ) : SearchRepository {
-    override suspend fun getSearchResult(query1: String, query2: String, query3: String): List<GroupEntity> {
+    override suspend fun getSearchResult(
+        query1: String,
+        query2: String,
+        query3: String
+    ): List<GroupEntity> {
         val query3List = query3.replace(" ", ",").split(",")
         val storeSnapshot = fireStore.collection("groups")
-            .whereArrayContains("category", query1)
-            .whereArrayContains("works", query2)
-            .whereArrayContainsAny("language", query3List).get().await()
+            .whereEqualTo("category.classification", query1)
+            .whereArrayContains("category.developmentOccupations", query2).get().await()
 
-        val response = storeSnapshot.documents.map { it.toObject(GroupResponse::class.java) }
+        val response = storeSnapshot.documents.map {
+            it.toObject(GroupResponse::class.java) ?: GroupResponse()
+        }.filter {
+            true
+            //아래 코드로 검색어 필터링
+            //it.category.programingLanguage.any{ it in query3List }
+        }
 
-        return response.map { it?.toEntity()?:GroupEntity() }
+        return response.map { it.toEntity() }
     }
 
 }
