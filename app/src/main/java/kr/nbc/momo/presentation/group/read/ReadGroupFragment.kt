6137aed268bdb339cb4916f9dd4824372712286a
WinterWood0118 <dltjdals125@gmail.com>
2024-06-13 09:32:40 +0900

    feat: Read UiState 적용완료

--- app/src/main/java/kr/nbc/momo/presentation/group/read/ReadGroupFragment.kt
+++ app/src/main/java/kr/nbc/momo/presentation/group/read/ReadGroupFragment.kt
@@ -27,7 +27,6 @@ import androidx.recyclerview.widget.GridLayoutManager
 import coil.load
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.chip.Chip
-import com.google.android.material.chip.ChipDrawable
 import com.google.android.material.chip.ChipGroup
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -41,6 +40,7 @@ import kr.nbc.momo.presentation.group.model.GroupModel
 import kr.nbc.momo.presentation.main.SharedViewModel
 import kr.nbc.momo.presentation.onboarding.signup.SignUpFragment
 import kr.nbc.momo.util.setThumbnailByUrlOrDefault
+import kr.nbc.momo.util.setVisibleToError
 import kr.nbc.momo.util.setVisibleToGone
 import kr.nbc.momo.util.setVisibleToVisible
 import java.util.Calendar
@@ -129,19 +129,23 @@ class ReadGroupFragment : Fragment() {
             viewModel.groupState.collect { uiState ->
                 when (uiState) {
                     is UiState.Loading -> {
-
+                        binding.prCircular.setVisibleToVisible()
+                        binding.svRead.setVisibleToGone()
                     }
 
                     is UiState.Success -> {
                         initView(uiState.data)
                         initGroupThumbnail(uiState.data.groupThumbnail)
                         initUserList(uiState.data.userList)
-
+                        binding.prCircular.setVisibleToGone()
+                        binding.svRead.setVisibleToVisible()
                     }
 
                     is UiState.Error -> {
                         // 오류 메시지 표시
                         Log.d("error", uiState.message)
+                        binding.prCircular.setVisibleToError()
+                        binding.svRead.setVisibleToGone()
                     }
                 }
 
