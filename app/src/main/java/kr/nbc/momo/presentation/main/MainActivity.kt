f4f4449048aab1a51be6f834fa6b6c21d5f7410c
Hyuna <kmtso434@naver.com>
2024-06-17 17:02:26 +0900

    {fix} icon_setup

--- app/src/main/java/kr/nbc/momo/presentation/main/MainActivity.kt
+++ app/src/main/java/kr/nbc/momo/presentation/main/MainActivity.kt
@@ -58,6 +58,7 @@ class MainActivity : AppCompatActivity() {
                         .replace(R.id.fragment_container, chattingListFragment)
                         .commit()
                     true
+
                 }
 
                 R.id.myPageFragment -> {
