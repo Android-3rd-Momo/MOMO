355a5e81773dde97e6dfa672d13b4d358c64be57
Combro-Kim <rlawog@nate.com>
2024-06-20 20:58:54 +0900

    feat: version3 마무리

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "kr.nbc.momo"
         minSdk = 33
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
