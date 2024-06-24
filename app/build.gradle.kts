6d8c3a9d6015d3352b838e408a6ef5247f197d13
Combro-Kim <rlawog@nate.com>
2024-06-24 14:09:29 +0900

    feat: version 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "kr.nbc.momo"
         minSdk = 33
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 6
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
